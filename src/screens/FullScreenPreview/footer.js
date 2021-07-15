2600d7829193b9fb8efc34034f63f360bacfd3ea
Đinh Văn Đức <ducdv.bk94@gmail.com>
2021-07-16 00:41:15 +0700

    Add lib to set live wallpaper

--- src/screens/FullScreenPreview/footer.js
+++ src/screens/FullScreenPreview/footer.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react';
-import {ActivityIndicator, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, Alert, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import GifWallpaper from 'react-native-gif-wallpaper';
 import ManageWallpaper, {TYPE} from 'react-native-manage-wallpaper';
 import OptionsMenu from 'react-native-option-menu';
 import Feather from 'react-native-vector-icons/Feather';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import {downloadImage} from '../../utils/filedownload';
+import {interstitial} from '../../utils/admob/Interstitial';
+import {downloadGifImage, downloadImage} from '../../utils/filedownload';
 import {showToastWithGravityAndOffset} from '../../utils/toast';
 import styles from './styles';
 
@@ -12,20 +15,74 @@ const OptionsFooter = (props) => {
   const [downloadState, setDownloadState] = useState(0);
   const [isSetting, setIsSetting] = useState(0);
 
+  const [loaded, setLoaded] = useState();
+  const [isGifImage, setIsGifImage] = useState();
+  const [imageDownload, setImageDownload] = useState();
+
+  useEffect(() => {
+    if (props.imageUrl.indexOf('.gif') != -1) {
+      setIsGifImage(true);
+    }
+    const eventListener = interstitial.onAdEvent((type) => {
+      if (type !== AdEventType.LOADED) {
+        setLoaded(false);
+        interstitial.load();
+      } else {
+        setLoaded(true);
+      }
+    });
+
+    interstitial.load();
+
+    return () => {
+      eventListener();
+    };
+  }, []);
+
+  const showInterstialAd = () => {
+    if (loaded) {
+      try {
+        if (!__DEV__) interstitial.show();
+      } catch (error) {
+        console.log(error.message);
+        interstitial.load();
+      }
+    }
+  };
+
   const onDownloadHandler = async () => {
+    showInterstialAd();
     setDownloadState(1);
-    const res = await downloadImage(props.imageUrl);
-    if (res) {
+    const res = isGifImage
+      ? await downloadGifImage(props.imageUrl)
+      : await downloadImage(props.imageUrl);
+    if (!!res) {
       showToastWithGravityAndOffset('Saved image');
       setDownloadState(2);
     } else {
       setDownloadState(0);
     }
+    return res;
   };
 
   const _callback = () => {
     setIsSetting(false);
     showToastWithGravityAndOffset('Wallpaper was set');
+    showInterstialAd();
+  };
+
+  const setLiveWallpaperHandler = () => {
+    Alert.alert('Set live wallpaper', 'Need to download gif before setting..', [
+      {text: 'Cancel'},
+      {
+        text: 'Ok',
+        onPress: async () => {
+          const filePath = await onDownloadHandler();
+          const uri = 'file://' + filePath;
+          GifWallpaper.setUpLiveWallpaper(uri);
+        },
+      },
+    ]);
   };
 
   const setWallpaperHandler = (type) => {
@@ -42,20 +99,31 @@ const OptionsFooter = (props) => {
   return (
     <View style={styles.bottomContainer}>
       <View style={styles.textInputContainer}>
-        <OptionsMenu
-          customButton={
+        {isGifImage ? (
+          <TouchableOpacity onPress={setLiveWallpaperHandler}>
             <Text style={styles.textInput}>
               {isSetting ? 'Setting...' : 'Set as wallpaper'}
             </Text>
-          }
-          destructiveIndex={3}
-          options={['Home screen', 'Lock screen', 'Both', 'Cancel']}
-          actions={[
-            () => setWallpaperHandler(TYPE.HOME),
-            () => setWallpaperHandler(TYPE.LOCK),
-            () => setWallpaperHandler(TYPE.BOTH),
-          ]}
-        />
+          </TouchableOpacity>
+        ) : (
+          <OptionsMenu
+            customButton={
+              <Text style={styles.textInput}>
+                {isSetting ? 'Setting...' : 'Set as wallpaper'}
+              </Text>
+            }
+            onPress={() => {
+              console.log('On press');
+            }}
+            destructiveIndex={3}
+            options={['Home screen', 'Lock screen', 'Both', 'Cancel']}
+            actions={[
+              () => setWallpaperHandler(TYPE.HOME),
+              () => setWallpaperHandler(TYPE.LOCK),
+              () => setWallpaperHandler(TYPE.BOTH),
+            ]}
+          />
+        )}
       </View>
 
       <View style={styles.messageButton}>
--- src/utils/filedownload.js
+++ src/utils/filedownload.js
@@ -3,6 +3,33 @@ import RNFetchBlob from 'rn-fetch-blob';
 const {config, fs} = RNFetchBlob;
 const PictureDir = fs.dirs.PictureDir;
 
+export const getFileExtension = (filePath) => {
+  return filePath.substring(filePath.lastIndexOf('.') + 1);
+};
+
+export const downloadGifImage = async (image_URL) => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      ToastAndroid.show('Start downloading', 100);
+      const res = await config({
+        fileCache: true,
+        appendExt: getFileExtension(image_URL),
+      }).fetch('GET', image_URL);
+      const filePath = res.path();
+      return filePath;
+    } else {
+      ToastAndroid.show('Can not download', 100);
+      return null;
+    }
+  } catch (err) {
+    ToastAndroid.show(err.message, 100);
+    return null;
+  }
+};
+
 export const getFileName = (filePath) => {
   return filePath.substring(filePath.lastIndexOf('/') + 1);
 };
